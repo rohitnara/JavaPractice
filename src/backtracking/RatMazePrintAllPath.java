34b07e9b2c694155f9c849515c7fedb894e8c165
rohitnara <rohitnarayan046@gmail.com>
2020-09-18 01:44:12 +0530

    solution

--- src/backtracking/RatMazePrintAllPath.java
+++ src/backtracking/RatMazePrintAllPath.java
@@ -6,7 +6,7 @@ import java.util.Arrays;
 public class RatMazePrintAllPath {
     public static void main(String args[]){
         int maze[][]={
-                {1, 0, 1, 1},
+                {1, 1, 1, 1},
                 {1, 1, 0, 1},
                 {1, 0, 1, 1},
                 {1, 1, 1, 1}

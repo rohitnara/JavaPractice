cdee9979bfe014a92d413c2def7607e807e31741
rohitnara <rohitnarayan046@gmail.com>
2020-09-18 01:30:35 +0530

    solution

--- src/backtracking/RatMazePrintAllPath.java
+++ src/backtracking/RatMazePrintAllPath.java
@@ -6,10 +6,10 @@ import java.util.Arrays;
 public class RatMazePrintAllPath {
     public static void main(String args[]){
         int maze[][]={
-                {1, 1, 1, 1},
+                {1, 0, 1, 1},
                 {1, 1, 0, 1},
-                {1, 1, 1, 1},
-                {10, 1, 0, 1}
+                {1, 0, 1, 1},
+                {1, 1, 1, 1}
         };
         if(maze.length==0){
             System.out.println("No Solution");
@@ -19,24 +19,20 @@ public class RatMazePrintAllPath {
         solve(0,0,maze,maze.length,maze[0].length,op);
     }
     private static void solve(int i,int j,int[][] maze,int m,int n,String op){
-        if(!isSafe(i,j,m,n,maze)){
-            return;
-        }
         if(i==m-1&&j==n-1){
             System.out.println(op);
             return;
         }
-        //vis[i][j]=true;
-        //System.out.print(vis[i][j]);
-        if(isSafe(i,j+1,m,n,maze)) {
-            String op1 = op + 'R';
-            solve(i, j + 1, maze, m, n, op1);
+        if(isSafe(i,j,m,n,maze)) {
+            if (isSafe(i, j + 1, m, n, maze)) {
+                String op1 = op + 'R';
+                solve(i, j + 1, maze, m, n, op1);
+            }
+            if (isSafe(i + 1, j, m, n, maze)) {
+                String op2 = op + 'D';
+                solve(i + 1, j, maze, m, n, op2);
+            }
         }
-        if(isSafe(i+1,j,m,n,maze)) {
-            String op2 = op + 'D';
-            solve(i + 1, j, maze, m, n, op2);
-        }
-        //vis[i][j]=false;
     }
     private static boolean isSafe(int i,int j, int m,int n, int[][] maze){
         return (i>=0&&i<m&&j>=0&&j<n&&maze[i][j]==1)?true:false;
